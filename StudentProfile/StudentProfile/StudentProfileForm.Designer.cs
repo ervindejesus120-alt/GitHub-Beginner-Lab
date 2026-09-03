919100a5f1f7c7f8f62447c1b3f3394e02d34014
Ervin Dejesus <ervindejesus120@gmail.com>
2026-09-03 14:55:30 +0800

    Add StudentProfile Visual Studio project

---
+++ StudentProfile/StudentProfile/StudentProfileForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace StudentProfile
+{
+    partial class StudentProfileForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(222, 150);
+            button1.Name = "button1";
+            button1.Size = new Size(334, 121);
+            button1.TabIndex = 0;
+            button1.Text = "Show Message";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // StudentProfileForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "StudentProfileForm";
+            Text = "StudentProfileForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
