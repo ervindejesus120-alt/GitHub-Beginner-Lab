2264e2c0549242874a358e7208cee8f24d0b051e
Ervin Krystoff De Jesus <User@DeJesus>
2026-09-25 00:14:42 +0800

    I applied the country code for mobile no.

--- StudentProfile/StudentProfile/StudentProfileForm.cs
+++ StudentProfile/StudentProfile/StudentProfileForm.cs
@@ -9,7 +9,7 @@ namespace StudentProfile
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(" Contact Number: 09384756381 ", "Student Profile!");
+            MessageBox.Show(" Contact Number: +63 938 475 6381", "Student Profile!");
         }
     }
 }
